6bfabe15869bca4a09eee76fde0dc51d3d3c24a9
carlosaw <carlosfreevg@gmail.com>
2023-07-26 16:30:47 -0400

    .them.catch.finally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,37 @@ import { useEffect, useState } from "react";
 
 const Page = () => {
   const [ users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   // Faz a requisição.
   useEffect(() => {
+    //console.log("Etapa 1");
+    //console.log("Etapa 2");
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
+      .then(res => res.json())
       .then(json => {
         setUsers(json);
       })
+      .catch(() => {
+        console.log("DEU ALGO ERRADO!");
+      })
+      .finally(() => {
+        setLoading(false);
+        //console.log("TERMINOU TODA A REQUISIÇÂO...")
+      });
+      //console.log("Etapa 4");
   }, []);
   
   return ( 
   
     <div className="container mx-auto w-96">
       <h1 className="text-3xl text-center mt-10">Lista de usuários</h1>
-      
+      {loading && "Carregando..."}
+      {!loading && users.length > 0 &&
         <ul>
           {users.map(item => (
             <li key={item.id} className="border-b p-4">
-              <p className="text-blue-500">{item.name}</p> 
+              <p className="text-2xl">{item.name}</p> 
               EMAIL: {item.email} 
               <p>END: Rua {item.address.street}</p>
               CIDADE: {item.address.city}
@@ -32,7 +44,8 @@ const Page = () => {
             </li>
           ))}
         </ul>
-      
+      } 
+      {!loading && users.length === 0 && "Não há usuários para exibir!"}     
     </div>      
   );  
 }
