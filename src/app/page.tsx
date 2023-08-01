cd32a2ff6838636921770640620e3888aff5b40c
carlosaw <carlosfreevg@gmail.com>
2023-07-31 21:11:23 -0400

    axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,32 @@
 "use client";
 
-import { User } from "@/types/User";
-import { useEffect, useRef, useState } from "react";
+import { api } from "@/utils/api";
 
 const Page = () => {
-  
-  const [legendInput, setLegendInput] = useState('');
-  const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleFileSend = async () => {
-
-    //console.log(fileInputRef.current?.files);
-    if(fileInputRef.current?.files && fileInputRef.current.files.length > 0) {
-      // Pega o arquivo escolhido
-      const fileItem  = fileInputRef.current.files[0];
-      console.log(fileItem);
-
-      // cria mimetypes
-      const allowed = ['image/jpg', 'image.jpeg', 'image/png'];
-      
-      if(allowed.includes(fileItem.type)) {
-        
-        const data = new FormData();
-        data.append('image', fileItem);
-        data.append('legend', legendInput);
-
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'multipart/form-data'
-          },
-          body: data
-      });
-      const json = await res.json();
-      console.log(json);
-
-      } else {
-        alert("Arquivo incompatível");
-      }
-
-    } else {
-      alert('Selecione um arquivo!');
-    }
+  const handleGetPost = async () => {
+    const response = await api.get('/posts',);
+    console.log(response);
   }
 
+  const handleAddPost = async () => {
+    const response = await api.post('/posts', {
+      userId: 98,
+      title: 'titulo do post',
+      body: 'corpo do post'
+    });
+    if(response.data.id) {
+      console.log('Inseriu beleza');
+    } else {
+      console.log('Não houve inserção!');
+    }
+    
+  }
+  
   return (  
     <div className="container mx-auto flex flex-col items-center">
-      <h1 className="text-3xl text-center mt-10 mb-10">Upload de Imagens</h1>
-
-      <div className="max-w-md flex flex-col gap-4 border border-dotted border-white p-3 mt-2">
-        <input 
-          type="file"
-          ref={fileInputRef}
-        /> 
-        <input
-          type="text" 
-          placeholder="Digite uma legenda"
-          className="p-3 bg-white rounded-md text-black"
-          value={legendInput}
-          onChange={e => setLegendInput(e.target.value)}
-        />
-        <button onClick={handleFileSend}>Enviar imagem</button>
-      </div>
+      <button onClick={handleAddPost}>Adicionar Post</button>
+      <button onClick={handleGetPost}>Pegar Posts</button>
     </div>      
   );  
 }
